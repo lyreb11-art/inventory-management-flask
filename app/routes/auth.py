3dcd7188816adf64d28097b36fb078ef33124dc3
lyreb11-art <lyreb11@gmail.com>
2026-07-06 14:33:51 +0000

    Fix Werkzeug compatibility

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
-from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 from app import db
 from app.models import User
 from app.forms import LoginForm, RegistrationForm
@@ -23,7 +23,7 @@ def login():
         
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('main.dashboard')
         
         flash(f'Welcome back, {user.username}!', 'success')
